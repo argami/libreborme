3f831da4d651bbe400d5398aa1d34629ca34625d
Gamaliel Toro <argami@gmail.com>
2018-12-07 13:27:46 +0000

    log directory in /tmp for now (dokku/heroku)

--- libreborme/settings.py
+++ libreborme/settings.py
@@ -172,7 +172,7 @@ BORME_PDF_ROOT = os.path.join(BORME_ROOT, 'pdf')
 BORME_XML_ROOT = os.path.join(BORME_ROOT, 'xml')
 BORME_JSON_ROOT = os.path.join(BORME_ROOT, 'json')
 
-BORME_LOG_ROOT = os.path.join(BASE_DIR, '..', 'log')
+BORME_LOG_ROOT = os.path.join('/tmp')
 
 EMAIL_CONTACT = 'contact@domain'
 
